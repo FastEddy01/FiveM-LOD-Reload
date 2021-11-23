20a0e8fc8f477dcfe2ff50c1342bce90849180f6
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2021-11-23 10:42:57 -0600

    Add files via upload

---
+++ Refresh_LOD/fxmanifest.lua
@@ -0,0 +1,112 @@
+-- This resource is part of the default Cfx.re asset pack (cfx-server-data)
+-- Altering or recreating for local use only is strongly discouraged.
+
+version '1.0.0'
+author 'Cfx.re <root@cfx.re>'
+description 'A flexible handler for game type/map association.'
+repository 'https://github.com/citizenfx/cfx-server-data'
+
+client_scripts {
+    "Client/*.lua",
+
+}
+
+fx_version 'adamant'
+games { 'gta5', 'rdr3' }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--AntiStop: True
